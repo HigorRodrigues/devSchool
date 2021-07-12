ef21e672a4518899492d6685d7113c2175072d7d
HigorRodrigues <higor.italva@gmail.com>
2021-07-11 22:14:43 -0300

    Primeiro commit do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PagePrincipalComponent } from './features/page-principal/page-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PagePrincipalComponent
   ],
   imports: [
     BrowserModule
